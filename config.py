26a22576d34d75dab573dab2585af93bbe46f26c
goat587 <goatgoat0987@gmail.com>
2025-01-27 17:25:32 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 api_id = 22696222
 api_hash = "1b4cdb255f37262200981dbbf87a1fa0"
 bot_token = "7897731857:AAG6GtiqGXlxn3NPVwlSJL713wlTFSnIwW8"
-auth_users = "46513214"
+auth_users = "7413628447"
 sudo_users = [int(num) for num in auth_users.split(",")]
 osowner_users = "7413628447"
 owner_users = [int(num) for num in osowner_users.split(",")]
