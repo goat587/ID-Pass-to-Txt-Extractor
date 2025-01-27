a6e05632ec9b7a390909f95bfc5444527835e7ed
goat587 <goatgoat0987@gmail.com>
2025-01-27 17:15:45 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
-api_id = 28660719
-api_hash = "34ade52147ebcaf4bc0b88aa3dfc7e1a"
-bot_token = "7001245667:AAGm3VkNigvYgO5nrB9Civflz9lop0B91d8"
-auth_users = "6990313439"
+api_id = 22696222
+api_hash = "1b4cdb255f37262200981dbbf87a1fa0"
+bot_token = "7897731857:AAG6GtiqGXlxn3NPVwlSJL713wlTFSnIwW8"
+auth_users = "7413628447"
 sudo_users = [int(num) for num in auth_users.split(",")]
-osowner_users = "6990313439"
+osowner_users = "7413628447"
 owner_users = [int(num) for num in osowner_users.split(",")]
